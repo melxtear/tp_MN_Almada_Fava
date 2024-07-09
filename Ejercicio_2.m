9f07eb1ccd8b7006e86a08d50be1ef2392b16e46
Mel Almada <101682418+melxtear@users.noreply.github.com>
2024-07-09 19:01:11 -0300

    Ejercicio 2
    Hicimos el ej2 (con algoritmo utilizado en 1), le agregamos el resultado analítico y buscamos en el vector X el resultado computacional en la última posición

--- Ejercicio_2.m
+++ Ejercicio_2.m
@@ -1,7 +1,3 @@
-%Calcule la posición de la masa m2, es decir, xm2(t) medida desde su centro de masa, cuando el sistema 
-%parte del equilibrio. Tenga presente para su análisis, un tiempo inicial t0 = 0 s, tf = 25 s y una resolución 
-%en el cálculo de la ecuación diferencial ?t = 1 ms.
-
 Inicializacion_Variables
 t0 = 0 ; % Tiempo Inicial [s]
 tf = 25 ; % Tiempo Final [s]
@@ -10,7 +6,14 @@ x0 = [0 0] ; % Condiciones Iniciales
 h1 = 0.001;  % Pasos de solución en segundos (?t = 1 ms)
 M1 = (tf-t0)/h1; 
 [t, X]= Ec_Dif_Runge_Kutta_O4_Sistemas('Ec_Dif_1',t0, tf, x0, M1); 
-n=size(X);
+
+n=length(X);
+x_computacional=X(n);
+disp('Ultima posicion');
+x_computacional %nos dio 0.2002
+
+x_=(a*m1*g)/((a^2)*densidad*g+k*A);
+x_ %nos dio 0.2001
 y = X(:,1);  % Solución aproximada de la ecuación diferencial
 H=figure;
 set(H,'name','TP INTEGRADOR 2024 - Ejercicio 1','position',[20 50 1200 600],'NumberTitle','off');
